ca6035172e084c91a046c4b4bf1be9e96a706c92
HaseebIjaz <haseebijazi150172@gmail.com>
2024-02-24 07:59:56 +0500

    [master] :INIT + HERO Section

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React,{useState, useRef, Suspense} from 'react'
+import {Canvas, useFrame} from '@react-three/fiber';
+import {Points, PointMaterial, Preload} from '@react-three/drei';
+
+const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground
